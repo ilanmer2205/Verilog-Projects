c653f084ce6fc72e7d3cb9f1022c2072bef8319f
Ilan Mermelstein <ilanmer@gmail.com>
2024-09-29 20:19:40 +0300

    Update top.v

--- UART(in progress)/top.v
+++ UART(in progress)/top.v
@@ -1,1 +1,16 @@
 
+module top(
+    input clk,
+    input reset,
+    input rx,
+    output [7:0] data
+    );
+    
+    wire w_tick;
+    
+    baud_generator b_g (.clk(clk), .reset(reset), .tick(w_tick));
+    
+    reciever reciever1(.clk(clk), .tick(w_tick), .reset(reset), .rx(rx), .data(data));
+    
+endmodule
+
