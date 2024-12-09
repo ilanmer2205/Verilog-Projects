7d25ebf3791d09a13fcefb2abaf0b7325c401750
Ilan Mermelstein <ilanmer@gmail.com>
2024-12-09 17:55:36 +0200

    Update a_clk.v

--- 6) Happy Birthday Song/a_clk.v
+++ 6) Happy Birthday Song/a_clk.v
@@ -1,1 +1,26 @@
 
+module a_clk(
+    input clk,
+    input en,
+    output clk_440
+    );
+    
+    reg [16:0]counter=0;
+    reg r_clk_440=0;
+    
+    assign clk_440=r_clk_440;
+        
+    always@(posedge clk) begin
+        if (en) begin 
+            if (counter==113_636) begin
+                counter<=0;
+                r_clk_440<=~r_clk_440; end
+            else
+                counter<= counter +1;
+        end
+        else
+            counter<=0;
+    end
+    
+endmodule
+
